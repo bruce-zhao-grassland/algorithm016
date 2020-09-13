993049b3f8128af92563ef8d95345a9103aaf2d8
bruce-zhao-grassland <670342324@qq.com>
2020-09-13 12:43:03 +0800

    08:LeetCode-283.移动零

---
+++ Week_01/08-MoveZero.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if (null == nums || nums.length < 2) {
+            return;
+        }
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[index++] = nums[i];
+            }
+        }
+        for (; index < nums.length; index++) {
+            nums[index] = 0;
+        }
+    }
+}
